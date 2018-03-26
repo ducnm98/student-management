fc582652097ce38231d47fb3b0bc0283d9951399
John Michael <ducnm.john98@gmail.com>
2018-03-26 09:06:37 +0700

    a

--- config/db/sequelize.js
+++ config/db/sequelize.js
@@ -3,11 +3,9 @@ const db_config = require('./db');
 
 const { database, username, password, host, dialect } = db_config;
 
-const sequelize = new Sequelize(database, username, password, {
+module.exports = new Sequelize(database, username, password, {
     host: host,
     dialect: dialect, //Dialect needs to be explicitly given
     timezone: '+07:00' //Set the timezone
-});
-
-module.exports = sequelize;
+});;
   
