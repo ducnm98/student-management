fc79c873a94e6da7cd9efd6b029dcacaa1a2cf56
John Michael <ducnm.john98@gmail.com>
2018-03-26 09:11:13 +0700

    edit style conn

--- config/db/db.js
+++ config/db/db.js
@@ -1,9 +1,7 @@
-const configDB = {
-    database: "genkiax_test",
-    username: "genkiax",
-    password: "abc123456",
-    host: "genkiax.com",
-    dialect: "mysql"
+module.exports = {
+  database: "genkiax_test",
+  username: "genkiax",
+  password: "abc123456",
+  host: "genkiax.com",
+  dialect: "mysql"
 };
-
-module.exports = configDB;
--- config/models/index.js
+++ config/models/index.js
@@ -1,21 +1,21 @@
-const Sequelize = require('sequelize');
-const sequelize = require('../db/sequelize');
+const Sequelize = require("sequelize");
+const sequelize = require("../db/sequelize");
 
 const db = {
-    Student: sequelize.import('../models/Student'),
+  Student: sequelize.import("../models/Student")
 };
 
 //Build object for later usage
 const example = db.Student.build({
-    first_name: 'John',
-    last_name: 'Doe',
-    email: 'johndoe@generic.com',
-    password: '123',
-    gender: 'Male',
-    birthday: '1979-01-01',
-    faculty: 'IT',
-    address: '123 Palm St. CA',
-    phone_number: '0123456789',
+  first_name: "John",
+  last_name: "Doe",
+  email: "johndoe@generic.com",
+  password: "123",
+  gender: "Male",
+  birthday: "1979-01-01",
+  faculty: "IT",
+  address: "123 Palm St. CA",
+  phone_number: "0123456789"
 });
 
 //Asynchronous instance method to hash the password
