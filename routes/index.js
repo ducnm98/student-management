6da9a92b56146009bccf192d4231989ada4a0dfe
John Michael <ducnm.john98@gmail.com>
2018-03-26 09:05:43 +0700

    Edit style of coding

--- routes/index.js
+++ routes/index.js
@@ -8,15 +8,4 @@ router.get('/', function(req, res, next) {
   else res.redirect('/login');
 });
 
-//For testing purposes
-/*router.post('/test', function(req, res, next) {
-  Student
-  .findOne({ where: { email: req.body.email } })
-  .then(data => { data.compare(req.body.password, (result) => {
-    res.json(result);
-  }) })
-  .catch(error => next(error))
-
-});*/
-
 module.exports = router;
