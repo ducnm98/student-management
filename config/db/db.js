406bb89de41768a769b4d8905988dcedde05d6d6
John Michael <ducnm.john98@gmail.com>
2018-04-17 13:28:53 +0700

    d

--- config/db/db.js
+++ config/db/db.js
@@ -9,7 +9,7 @@
 
 // Connect to database in localhost
 module.exports = {
-  database: "nhapmoncongnghe",
+  database: "student_management",
   username: "root",
   password: "",
   host: "localhost",
